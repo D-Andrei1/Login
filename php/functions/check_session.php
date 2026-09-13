36a6b0660043c2432792233d81da8b7af43fca3d
Noob <darius.il@yahoo.com>
2026-09-13 02:57:53 +0100

    Fixed usernames not showing on client user menu - server wasnt sending it

--- php/functions/check_session.php
+++ php/functions/check_session.php
@@ -4,6 +4,7 @@ if (isset($_SESSION['user_id'])){
     echo json_encode([
         'user_id' => $_SESSION['user_id'],
         'logged_in' => $_SESSION['logged_in'],
+        'username' => $_SESSION['username']
     ]);
 } else {
     echo json_encode([
